4980d87f1c793a751d2b9a0d86ba3c0a236851b5
Unknown <jakeshmearlof4@gmail.com>
2018-03-06 12:30:55 -0700

    made scriptable objects change color on jump and run

---
+++ scriptable objects/Assets/Scripts/PoseButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New PoseButton", menuName = "PoseButton")]
+public class PoseButton : ScriptableObject {
+
+    public string Title;
+    public Color color;
+
+
+
+}
